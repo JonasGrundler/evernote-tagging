fd054a567e0b26ab94b8a1f117b73e78a23f87b3
Jonas Grundler <jonas@NSO-NB-CDVV444.localdomain>
2025-12-02 11:26:25 +0100

    include more tests

---
+++ java/runner/run_services_tests.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+if [[ -z "$VENV_PATH" ]]; then
+  VENV_PATH="$HOME/python-venv/.venv"
+fi
+SRC="$SCRIPT_DIR/../../python/src"
+TEST="$SCRIPT_DIR/src/test/resources"
+
+# ... initialisiert DATA (.../data_test) mit den Inhalten von DATA_SOURCE (.../data_source)
+source "$SRC/../test/f_prepare_tests.sh"
+prepare_tests_env
+
+# starte den server
+source "$SRC/f_server.sh"
+start_server_managed
+
+mvn test
--- java/runner/run_tests.sh
+++ java/runner/run_tests.sh
@@ -4,13 +4,6 @@ set -e
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-SRC="$SCRIPT_DIR/../../python/src"
-TEST="$SCRIPT_DIR/src/test/resources"
+source "$SCRIPT_DIR/f_prepare_tests.sh"
 
-# ... initialisiert DATA (.../data_test) mit den Inhalten von DATA_SOURCE (.../data_source)
-source $SRC/f_prepare_tests.sh
-
-export DATA
-$SRC/run_server.sh --bg
-
-mvn test
+mvn test -Dtest=EnexFilesProcessorTest
